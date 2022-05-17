4203a4f3435f0da8c174363f7e7ab68b20f7a611
blaksleyn <blaksleyn@gmail.com>
2022-05-16 23:52:40 -0300

    Actions

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import getToken from "../utilities/getToken";
 
-export async function  getGallery() {
+export async function getGallery() {
   return async function (dispatch) {
     const info = await axios.get(`http://localhost:1500/api/gallery`, {});
     console.log(info.data);
@@ -14,7 +14,11 @@ export async function  getGallery() {
 
 export function postGallery(filename) {
   return async function (dispatch) {
-    let info = await axios.post(`http://localhost:1500/api/gallery/save`, filename, getToken());
+    let info = await axios.post(
+      `http://localhost:1500/api/gallery/save`,
+      filename,
+      getToken()
+    );
     return dispatch({
       type: "POST_GALLERY",
       payload: info.data,
@@ -32,6 +36,19 @@ export function deleteGallery(archivo) {
     return dispatch({
       type: "DELETE_GALLERY",
       payload: info.data,
+    });
+  };
+}
+
+export function login(formulario) {
+  return async function (dispatch) {
+    let info = await axios.post(
+      "http://localhost:1500/api/users/login",
+      formulario
+    );
+    return dispatch({
+      type: "LOGIN",
+      payload: info.data.token,
     });
   };
 }
