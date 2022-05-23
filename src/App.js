dae3b5e78fbe2433ed100301e7b3b5555172d8f2
Totoblak90 <tobias.blaksley@hotmail.com>
2022-05-23 01:34:59 -0300

    terminamos la primer parte de la página

--- src/App.js
+++ src/App.js
@@ -16,40 +16,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  // const dispatch = useAppDispatch();
-  // const galleries = useAppSelector(selectGallery);
-  // const projects = useAppSelector(selectProjects);
-  // const assets = useAppSelector(selectAssets);
-  // const proyectById = useAppSelector(selectProjectsID);
-
-  // useEffect(() => {
-  //   const getGallery = () =>
-  //     axios
-  //       .get(`http://localhost:1500/api/gallery`)
-  //       .then((res) => dispatch(setGalleries(res.data)))
-  //       .catch(console.log);
-  //   const getProjects = () =>
-  //     axios
-  //       .get(`http://localhost:1500/api/projects`)
-  //       .then((res) => dispatch(getAllProjects(res.data)))
-  //       .catch(console.log);
-  //   const getAssets = (id = "8fdf88b8-14a2-448c-8804-144787befcbb") =>
-  //     axios
-  //       .get(`http://localhost:1500/api/assets/${id}`)
-  //       .then((res) => dispatch(getAllAssets(res.data)))
-  //       .catch(console.log);
-
-  //   const getProyectId = (id = "8fdf88b8-14a2-448c-8804-144787befcbb") =>
-  //     axios
-  //       .get(`http://localhost:1500/api/projects/${id}`)
-  //       .then((res) => dispatch(getOneProyect(res.data)))
-  //       .catch(console.log);
-
-  //   getGallery();
-  //   getProjects();
-  //   getAssets();
-  //   getProyectId();
-  // }, []);
 
   return (
     <BrowserRouter>
