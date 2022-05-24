6891818cc30ecd445473d0e0745c0ad93b463f82
Totoblak90 <tobias.blaksley@hotmail.com>
2022-05-24 19:23:41 -0300

    terminamos el componente de crear un proyecto nuevo

--- src/components/AdminCreateProyect/AdminCreateProyect.js
+++ src/components/AdminCreateProyect/AdminCreateProyect.js
@@ -1,11 +1,67 @@
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./AdminCreateProyect.css";
+
+
+
+const formValidation =  (proyecto) => {
+        let errors = {
+            name: "El campo de nombre es requerido. Debe tener un mínimo de 3 caracteres",
+            description: "El campo de descripción es requerido. Debe tener un mínimo de 10 caracteres",
+            proyect_date: "La fecha de inicio del proyecto es requerida.",
+            image: "La imágen es obligatoria.",
+        }
+
+        if (typeof proyecto.name === 'string' && proyecto.name !== "" && proyecto.name.length >= 3 ) {
+            errors.name = "";
+        }  
+
+        if (typeof proyecto.description === 'string' && proyecto.description !== "" && proyecto.description.length >= 10) {
+            errors.description = "";
+        }  
+
+        if (typeof proyecto.proyect_date === 'string' && proyecto.proyect_date !== "") {
+            errors.proyect_date = ""
+        }
+
+        if (proyecto.image) {
+            errors.image = ""
+        }
+
+        return errors;
+
+    //     let errors = {
+    //         name: "El campo de nombre es requerido. Debe tener un mínimo de 3 caracteres",
+    //         description: "El campo de descripción es requerido. Debe tener un mínimo de 10 caracteres",
+    //         image: "La imágen es obligatoria."
+    //     }
+
+    //     if (typeof proyecto.name === 'string' && proyecto.name !== "" && proyecto.name.length >= 3 ) {
+    //         errors.name = "";
+    //     }  
+        
+    //     if (typeof proyecto.description === 'string' && proyecto.description !== "" && proyecto.description.length >= 10) {
+    //         errors.description = "";
+    //     }  
+
+    //     if (proyecto.image) {
+    //         errors.image = ""
+    //     }
+
+    //     return errors
+}
+
+
 
 function AdminCreateProyect() {
     const navigate = useNavigate();
     const [proyecto, setProyecto] = useState({name: "", image: "", proyect_date: "", description: ""});
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        setErrors(formValidation(proyecto));
+    }, [proyecto])
 
     const createProyect = (body) => {
         const formData = new FormData();
@@ -36,21 +92,25 @@ function AdminCreateProyect() {
     }
 
     return <>
-        <section>
+        <section >
             <Link to="/admin">
                 <button>Volver</button>
             </Link>
             <div>
                 <form onSubmit={(e) => handleFormSubmit(e)}>
-                    <label htmlFor="proyect_name">Nombre</label>
-                    <input type="text" name="name" id="proyect_name" value={proyecto.name} onChange={(e) => handleInputChange(e)}></input>
-                    <label htmlFor="proyect_description">Descripción</label>
-                    <input type="text" name="description" id="proyect_description" value={proyecto.description} onChange={(e) => handleInputChange(e)}></input>
-                    <label htmlFor="proyect_date">Fecha</label>
-                    <input type="date" name="proyect_date" id="proyect_date" value={proyecto.proyect_date} onChange={(e) => handleInputChange(e)}></input>
-                    <label htmlFor="proyect_image">Fecha</label>
-                    <input type="file" name="image" id="proyect_image" onChange={(e) => handleFileInputChange(e)}></input>
-                    <button type="submit">Crear proyecto</button>
+                    <label htmlFor="proyect_name">Nombre:</label>
+                    <input type="text" name="name" id="proyect_name" value={proyecto.name} onChange={(e) => {handleInputChange(e)}}></input>
+                    <span>{errors.name && proyecto.name ? errors.name : ''}</span> 
+                    <label htmlFor="proyect_description">Descripción:</label>
+                    <input type="text" name="description" id="proyect_description" value={proyecto.description} onChange={(e) => {handleInputChange(e)}}></input>
+                    <span>{errors.description && proyecto.description ? errors.description : ''}</span> 
+                    <label htmlFor="proyect_date">Fecha:</label>
+                    <input type="date" name="proyect_date" id="proyect_date" value={proyecto.proyect_date} onChange={(e) => {handleInputChange(e)}}></input>
+                    <span>{errors.proyect_name && proyecto.proyect_name ? errors.proyect_name : ''}</span> 
+                    <label htmlFor="proyect_image">Imágen:</label>
+                    <input type="file" name="image" id="proyect_image" onChange={(e) => {handleFileInputChange(e)}}></input>
+                    <span>{errors.image && proyecto.image ? errors.image : ''}</span>
+                    <button type="submit" disabled={errors.name || errors.description || errors.proyect_date || errors.image}>Crear proyecto</button>
                 </form>
             </div>
         </section>
